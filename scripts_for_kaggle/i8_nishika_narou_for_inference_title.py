92945b891c0c3f448cfebb821922dc6affb6bc00
hfukuse <fukuhiro0420@yahoo.co.jp>
2021-12-07 00:34:28 +0900

    change settings path

--- scripts_for_kaggle/i8_nishika_narou_for_inference_title.py
+++ scripts_for_kaggle/i8_nishika_narou_for_inference_title.py
@@ -43,7 +43,7 @@ def make_parse():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
     arg("--debug", action="store_true", help="debug")
-    arg("--settings", default="./settings_for_kaggle.json", type=str, help="settings path")
+    arg("--settings", default="./nishika-narou-2021-1st-place-solution/settings_for_kaggle.json", type=str, help="settings path")
     arg("--is_test", action="store_true", help="test")
     return parser
 
