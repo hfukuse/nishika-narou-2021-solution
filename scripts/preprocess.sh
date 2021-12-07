d859ac693d61663d0798dca023fb50c64d7f4e1b
hfukuse <fukuhiro0420@yahoo.co.jp>
2021-12-07 15:23:01 +0900

    fix scripts

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,5 +1,5 @@
 NAROU_DIR=./nishika-narou-2021-1st-place-solution
 
-python ${NAROU_DIR}/scripts_for_kaggle/make_k_fold.py
-python ${NAROU_DIR}/scripts_for_kaggle/concat_pos_with_k_fold.py
-python ${NAROU_DIR}/scripts_for_kaggle/concat_features_with_posk_fold.py
+python ${NAROU_DIR}/scripts/make_k_fold.py
+python ${NAROU_DIR}/scripts/concat_pos_with_k_fold.py
+python ${NAROU_DIR}/scripts/concat_features_with_posk_fold.py
