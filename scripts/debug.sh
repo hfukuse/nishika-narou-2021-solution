dc844469bf6eaa2836124abe6a53026e0a4e4258
hfukuse <fukuhiro0420@yahoo.co.jp>
2021-12-07 22:31:26 +0900

    fix cd

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,6 +1,4 @@
-SCRIPT_DIR=./scripts
-
-cd ./nishika-narou-2021-1st-place-solution
+SCRIPT_DIR=./nishika-narou-2021-1st-place-solution/scripts
 
 #i8_modelを作成
 python ${SCRIPT_DIR}/n8_nishika_narou_for_train.py --debug
